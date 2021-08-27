a7a7fe39e0b3efc15bf9e172a279f8d586b8adcc
bob <pdkwik@gmail.com>
2021-08-27 09:25:29 -0400

    server2.py as taken from PasteBin and modified for word wraps..

---
+++ issue044/client2.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/env python
+# client2.py
+
+from socket import *
+from time import time
+from time import sleep
+import sys
+BUFSIZE = 4096
+
+class CmdLine:
+    def __init__(self,host):
+        self.HOST = host
+        self.PORT = 21000
+        self.ADDR = (self.HOST, self.PORT)
+        self.sock = None
+
+    def makeConnection(self):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.connect(self.ADDR)
+
+    def sendCmd(self, cmd):
+        print("IN SENDCMD cmd = ", cmd)
+        self.sock.send(cmd.encode())
+
+    def getResults(self):
+        data = self.sock.recv(BUFSIZE)
+        print("RECEIVED DATA = ", data.decode())
+
+if __name__ == '__main__':
+    conn = CmdLine('puccini')
+    conn.makeConnection()
+    conn.sendCmd('ls -l')
+    conn.getResults()
+    conn.makeConnection()
+    conn.sendCmd('BYE')
+    conn.getResults()
