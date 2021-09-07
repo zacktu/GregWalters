1d2da6410091782f9b2199939c31515b1b42beaa
bob <pdkwik@gmail.com>
2021-09-07 12:04:45 -0400

    First two examples from issue51 programming with Tkinter.

--- issue051/example1.py
+++ issue051/example1.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
+
 # Example 1
 from tkinter import *
 
 root = Tk()
-button = Button(root, text='Hello Fullcircle').grid()
-
+button = Button(root, text="Hello FullCircle").grid()
 root.mainloop()
--- issue051/example2.py
+++ issue051/example2.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python3
+# Example 2
 
-# Example 1
 from tkinter import *
 
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        self.lblText = Label(frame, text="This is a label widget")
+        self.btnQuit = Button(frame, text="Quit", fg="red", command=frame.quit)
+        self.btnHello = Button(frame, text="Hello", command=self.SaySomething)
+
+        frame.grid(column=0, row=0)
+        self.lblText.grid(column=0, row=0, columnspan=2)
+        self.btnHello.grid(column=0, row=1)
+        self.btnQuit.grid(column=1, row=1)
+
+    def SaySomething(self):
+        print("Hello to FullCircle Magazine Readers!!!!!!!!!!")
+
+
 root = Tk()
-button = Button(root, text="Hello FullCircle").grid()
+app = App(root)
 root.mainloop()
